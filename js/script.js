5914aefaaaf57169f426d616b541625460e7a74e
Your Name <almannofrancesco@gmail.com>
2022-12-12 10:09:08 +0100

    grid system of one element created

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ alert('Benvenuto Player 1! Pronto per una nuova entusiasmante partita del famoso
 
 let nome = prompt("Per favore inserisci il tuo nome");
 console.log(nome);
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+
